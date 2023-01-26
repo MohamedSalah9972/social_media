acd830f7b24ae28c51ba34d2512b349089aceb15
Mohammed Salah <mohamed.salah9972@gmail.com>
2023-01-26 20:00:27 +0200

    Register apps

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SocialUser
 
-# Register your models here.
+class AdminSocialUser(admin.ModelAdmin):
+    pass
+
+admin.site.register(SocialUser, AdminSocialUser)
