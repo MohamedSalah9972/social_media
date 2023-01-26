648b2502482f76fa2b406a6dc78b2cbb789a9e68
Mohammed Salah <mohamed.salah9972@gmail.com>
2023-01-26 17:31:31 +0200

    Add Friend request model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from model_utils.models import TimeStampedModel
 
 
 class SocialUser(AbstractUser):
@@ -12,3 +14,11 @@ class Profile(AbstractUser):
 
     def __str__(self):
         return str(self.user.username)
+
+
+class FriendRequest(TimeStampedModel):
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='from_user', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='to_user', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str("Friend request from {}, to {}").format(self.from_user.username, self.to_user.username)
